6e171cb4a6e272674182206a21cfffa94b381855
dc-coder <mir.mir@gmail.com>
2017-05-22 11:50:21 -0400

    creating tender stage section

--- js/ui.js
+++ js/ui.js
@@ -6,4 +6,38 @@ $(".mdc-tab").click(function() {
 
 });
 
+$(function () { 
+    var myChart = Highcharts.chart('chart-tender', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+        	text: ''
+        },
+        xAxis: {
+            categories: ['Estimated Cost', 'Budget Amount']
+        },
+        yAxis: {
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+        	enabled: false
+    	},
+    	credits: {
+		      enabled: false
+		},
+		colors: ['#0bb586', '#008459'],
+        series: [{
+            name: 'tender',
+            data: [19.16, 25],
+            colorByPoint: true
+        } ]
+    });
+
+    myChart.setSize(250, 250);
+
+});
+
  
